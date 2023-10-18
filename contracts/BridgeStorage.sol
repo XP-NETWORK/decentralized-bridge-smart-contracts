ca2561b77f5f2c706fea6ed27669d650ec88fadd
zOthix <4bstractanimation@gmail.com>
2023-10-18 10:24:18 +0500

    Add comments and Update BridgeStorage for usecase

--- contracts/BridgeStorage.sol
+++ contracts/BridgeStorage.sol
@@ -3,73 +3,124 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+/**
+ * @dev Struct representing details of an NFT transfer between chains.
+ */
 struct NftTransferDetails {
-    uint256 id;
-    string sourceChain;
-    string destinationChain;
-    string sourceUserAddress;
-    string destinationUserAddress;
-    string sourceNftContractAddress;
-    string metadata;
-    string transactionHash;
-    uint256 count;
-    string nftType;
+    uint256 id; // Unique ID for the NFT transfer
+    string sourceChain; // Chain from where the NFT is being transferred
+    string destinationChain; // Chain to where the NFT is being transferred
+    string sourceUserAddress; // User's address in the source chain
+    string destinationUserAddress; // User's address in the destination chain
+    string sourceNftContractAddress; // Address of the NFT contract in the source chain
+    string metadata; // Metadata related to the NFT being transferred
+    string transactionHash; // Transaction hash of the transfer on the source chain
+    uint256 count; // Number of NFTs being transferred
+    string nftType; // Type of the NFT (could be ERC721 or ERC1155)
 }
 
+/**
+ * @dev Struct representing an NFT transfer with associated signatures.
+ */
 struct NftTransferWithSignatures {
-    NftTransferDetails transferDetails;
-    string[] signatures;
+    NftTransferDetails transferDetails; // Details of the NFT transfer
+    string[] signatures; // Signatures associated with the transfer
 }
 
-
+/**
+ * @title BridgeStorage
+ * @dev A contract to store and manage cross-chain transfer signatures and details.
+ */
 contract BridgeStorage {
     
-    mapping(string => string[]) public stakingSignatures; // Chain_TxHash => Signatures []
-    mapping(string=> NftTransferWithSignatures ) public lockSignatures; // Chain_TxHash => Signatures []
+    // Mapping from staker's address to an array of their signatures.
+    mapping(address => string[]) public stakingSignatures;
+    
+    // Mapping from a concatenated string of chain and transaction hash to the transfer details and its signatures.
+    mapping(string => NftTransferWithSignatures) public lockSignatures;
 
-    mapping(string => bool) public usedSignatures; // Signature => Used / Unused
+    // Mapping to check if a signature has already been used.
+    mapping(string => bool) public usedSignatures;
 
-
+    /**
+     * @dev Concatenates two strings.
+     * @param a First string.
+     * @param b Second string.
+     * @return Concatenated string.
+     */
     function concatenate(string memory a, string memory b) public pure returns (string memory) {
         return string(abi.encodePacked(a, b));
     }
 
-
-    function approveStake( string calldata txHash, string calldata chain,string calldata signature) public  {
-        require(usedSignatures[signature] != true, "Signature already used");
+    /**
+     * @dev Approves a stake using a signature.
+     * @param stakerAddress Address of the staker.
+     * @param signature The signature to be approved.
+     */
+    function approveStake(address stakerAddress, string calldata signature) public {
+        require(!usedSignatures[signature], "Signature already used");
         usedSignatures[signature] = true;
-        stakingSignatures[concatenate(txHash, chain)].push(signature);
+        stakingSignatures[stakerAddress].push(signature);
     }
 
-    function getStakingSignatures ( string calldata txHash, string calldata chain) external view returns ( string[] memory signatures ) {
-        return  stakingSignatures[concatenate(txHash, chain)];
-    } 
+    /**
+     * @dev Retrieves staking signatures for a specific staker.
+     * @param stakerAddress Address of the staker.
+     * @return Array of signatures.
+     */
+    function getStakingSignatures(address stakerAddress) external view returns (string[] memory) {
+        return stakingSignatures[stakerAddress];
+    }
 
-    function getStakingSignaturesCount ( string calldata txHash, string calldata chain) external view returns ( uint256 signaturesCount ) {
-        return  stakingSignatures[concatenate(txHash, chain)].length;
-    } 
+    /**
+     * @dev Retrieves the count of staking signatures for a specific staker.
+     * @param stakerAddress Address of the staker.
+     * @return Number of signatures.
+     */
+    function getStakingSignaturesCount(address stakerAddress) external view returns (uint256) {
+        return stakingSignatures[stakerAddress].length;
+    }
 
+    /**
+     * @dev Approves the locking of an NFT using a signature and associated transfer details.
+     * @param nftTransferDetails Details of the NFT transfer.
+     * @param signature The signature to be approved.
+     */
     function approveLockNft(NftTransferDetails calldata nftTransferDetails, string calldata signature) public {
-        require(usedSignatures[signature] != true, "Signature already used");
+        require(!usedSignatures[signature], "Signature already used");
         usedSignatures[signature] = true;
 
-         if (bytes(lockSignatures[nftTransferDetails.transactionHash].transferDetails.transactionHash).length == 0) {
-            // If not, initialize it with the provided transferDetails and an empty signatures array
-            lockSignatures[concatenate(nftTransferDetails.transactionHash, nftTransferDetails.sourceChain)] = NftTransferWithSignatures({
+        string memory chainAndTxHash = concatenate(nftTransferDetails.sourceChain, nftTransferDetails.transactionHash);
+
+        if (bytes(lockSignatures[chainAndTxHash].transferDetails.transactionHash).length == 0) {
+            lockSignatures[chainAndTxHash] = NftTransferWithSignatures({
                 transferDetails: nftTransferDetails,
                 signatures: new string[](0)
             });
         }
-        
-        lockSignatures[concatenate(nftTransferDetails.transactionHash, nftTransferDetails.sourceChain)].signatures.push(signature);
+
+        lockSignatures[chainAndTxHash].signatures.push(signature);
     }
 
-    function getLockNftSignatures ( string calldata txHash, string calldata chain) external view returns ( NftTransferWithSignatures memory signaturesWithTransferDetails ) {
-        return  lockSignatures[concatenate(txHash, chain)];
-    } 
+    /**
+     * @dev Retrieves lock signatures and associated transfer details for a specific chain and transaction.
+     * @param chain Source chain of the transfer.
+     * @param txHash Transaction hash of the transfer.
+     * @return Struct containing transfer details and associated signatures.
+     */
+    function getLockNftSignatures(string calldata chain, string calldata txHash) external view returns (NftTransferWithSignatures memory) {
+        string memory chainAndTxHash = concatenate(chain, txHash);
+        return lockSignatures[chainAndTxHash];
+    }
 
-    function getLockNftSignaturesCount ( string calldata txHash, string calldata chain) external view returns ( uint256 signaturesCount ) {
-        return  lockSignatures[concatenate(txHash, chain)].signatures.length;
-    } 
-    
+    /**
+     * @dev Retrieves the count of lock signatures for a specific chain and transaction.
+     * @param chain Source chain of the transfer.
+     * @param txHash Transaction hash of the transfer.
+     * @return Number of signatures.
+     */
+    function getLockNftSignaturesCount(string calldata chain, string calldata txHash) external view returns (uint256) {
+        string memory chainAndTxHash = concatenate(chain, txHash);
+        return lockSignatures[chainAndTxHash].signatures.length;
+    }
 }
--- contracts/XpStaking.sol
+++ contracts/XpStaking.sol
@@ -3,30 +3,51 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-// Uncomment this line to use console.log
+// Uncomment the following line if you want to use the hardhat console.log for debugging purposes.
 // import "hardhat/console.sol";
 
-
+/**
+ * @title XpStaking
+ * @dev A contract for staking a specific amount of XP tokens.
+ */
 contract XpStaking {
+    
+    // The required amount for staking.
     uint256 public stakingAmount;
+    
+    // The token being staked.
     IERC20 public xpToken;
+    
+    // Mapping to track the staking balances of individual addresses.
     mapping(address => uint256) public stakingBalances;
 
-
+    // Event emitted when a user stakes tokens.
     event Staked(address indexed user, uint256 amount);
 
-
+    /**
+     * @dev Contract constructor that initializes the staking amount and token contract.
+     * @param _stakingAmount Amount that is required to stake.
+     * @param _erc20Token Address of the ERC20 token to be staked.
+     */
     constructor(uint256 _stakingAmount, address _erc20Token) {
         stakingAmount = _stakingAmount;
         xpToken = IERC20(_erc20Token);
     }
 
+    /**
+     * @dev Allows users to stake a specific amount of XP tokens.
+     * @param amount The amount of tokens to be staked.
+     */
     function stakeXP(uint256 amount) public {
         require(amount == stakingAmount, "Staking amount must be equal to the predefined staking amount.");
-        
-        xpToken.transferFrom(msg.sender, address(this), stakingAmount);
 
+        // Transfer the staking amount from the staker to this contract.
+        xpToken.transferFrom(msg.sender, address(this), stakingAmount);
+        
+        // Update the staking balance for the staker.
         stakingBalances[msg.sender] += stakingAmount;
+
+        // Emit a staking event.
         emit Staked(msg.sender, stakingAmount);
     }
 }
