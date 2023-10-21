d5270f7daca1af87295a83ef1487a425d147ebb2
zOthix <4bstractanimation@gmail.com>
2023-10-21 22:53:53 +0500

    update deploy script of bridge

--- scripts/deployBridge.ts
+++ scripts/deployBridge.ts
@@ -2,9 +2,24 @@ import { ethers } from "hardhat";
 
 async function main() {
 
+    const collectionDeployerContract = await ethers.deployContract("NFTCollectionDeployer");
+    await collectionDeployerContract.waitForDeployment();
+    const collectionDeployerContractAddress = collectionDeployerContract.target
+    console.log(
+        `NFTCollectionDeployer contract deployed at ${collectionDeployerContractAddress}`
+    );
+
+
+    const storageDeployerContract = await ethers.deployContract("NFTStorageDeployer");
+    await storageDeployerContract.waitForDeployment();
+    const storageDeployerContractAddress = storageDeployerContract.target
+    console.log(
+        `StorageDeployerContract contract deployed at ${storageDeployerContractAddress}`
+    );
+
     const bootstrapValidator = ["0x67081bD856e29d7D7B3028C34Afb331fa6b3186E"]
     const chainSymbol = "BSC"
-    const bridge = await ethers.deployContract("Bridge", [bootstrapValidator, chainSymbol]);
+    const bridge = await ethers.deployContract("Bridge", [bootstrapValidator, chainSymbol, collectionDeployerContractAddress, storageDeployerContractAddress]);
 
     await bridge.waitForDeployment();
 
