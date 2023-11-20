02907c30da956c6e83ec80162cbac527f12e6fce
D <ddisdead462@gmail.com>
2023-11-20 21:32:30 +0500

    new changes

--- ton/scripts/deployBridge.ts
+++ ton/scripts/deployBridge.ts
@@ -1,19 +1,20 @@
-import { Address, beginCell, toNano } from 'ton-core';
-import { Bridge } from '../wrappers/Bridge';
+import { Address, Dictionary, beginCell, toNano } from 'ton-core';
+import { Bridge, ClaimData, SignerAndSignature, storeClaimData } from '../wrappers/Bridge';
 import { NetworkProvider } from '@ton-community/blueprint';
 import { testKey } from '@tact-lang/emulator';
 import { NFTCollectionDeployer } from '../build/Bridge/tact_NFTCollectionDeployer';
 import { NftCollection } from '../build/Bridge/tact_NftCollection';
+import { sign } from 'ton-crypto';
 
 export async function run(provider: NetworkProvider) {
 
-    let key = testKey('wallet-ncccxxxertdddyuaamm');
+    let key = testKey('firstValidator');
     console.log("key", key.publicKey.toString("hex"));
     console.log("key", key.secretKey.toString("hex"));
+
     let publicKey = beginCell().storeBuffer(key.publicKey).endCell().beginParse().loadUintBig(256);
 
-    const bridge = provider.open(await Bridge.fromInit(publicKey, "TON"));
-    const CollectionDeployer = provider.open(await NFTCollectionDeployer.fromInit(bridge.address));
+    const bridge = provider.open(await Bridge.fromInit(publicKey, Address.parseFriendly("EQAV8tH2WDuWYU7zAmkJmIwP8Ph_uIC4zBqJNIfKgRUUQewh").address, "TON"));
 
     await bridge.send(
         provider.sender(),
@@ -25,8 +26,6 @@ export async function run(provider: NetworkProvider) {
 
     await provider.waitForDeploy(bridge.address);
 
-
-
     const OFFCHAIN_CONTENT_PREFIX = 0x01;
 
     const string_first = "https://s.getgems.io/nft-staging/c/628f6ab8077060a7a8d52d63/"; // Change to the content URL you prepared
@@ -35,6 +34,55 @@ export async function run(provider: NetworkProvider) {
 
     // The Transaction body we want to pass to the smart contract
     let body = beginCell().storeUint(0, 32).storeStringTail("Mint").endCell();
+
+
+    // let claimData: ClaimData = {
+    //     $$type: "ClaimData",
+    //     destinationChain: "TON",
+    //     destinationUserAddress: Address.parseFriendly("EQAV8tH2WDuWYU7zAmkJmIwP8Ph_uIC4zBqJNIfKgRUUQewh").address,
+    //     fee: 0n,
+    //     metadata: "https://s.getgems.io/nft-staging/c/628f6ab8077060a7a8d52d63/0.json",
+    //     name: "Gando",
+    //     nftType: "singular",
+    //     royalty: {
+    //         $$type: "RoyaltyParams",
+    //         denominator: 1000n,
+    //         destination: bridge.address,
+    //         numerator: 350n
+    //     },
+    //     royaltyReceiver: Address.parseFriendly("EQAV8tH2WDuWYU7zAmkJmIwP8Ph_uIC4zBqJNIfKgRUUQewh").address,
+    //     sourceChain: "BSC",
+    //     sourceNftContractAddress: "0x6f7C0c6A6dd6E435b0EEc1c9F7Bce01A1908f386",
+    //     symbol: "Gando",
+    //     tokenAmount: 1n,
+    //     tokenId: 64n,
+    //     transactionHash: "0x6f7C0c6A6dd6E435b0EEc1c9F7Bce01A1908f386"
+    // }
+
+    // let signature = sign(beginCell().store(storeClaimData(claimData)).endCell().hash(), key.secretKey);
+
+    // let sig: SignerAndSignature = {
+    //     $$type: 'SignerAndSignature',
+    //     key: publicKey,
+    //     signature: beginCell().storeBuffer(signature).endCell()
+    // };
+
+    // let dictA = Dictionary.empty<bigint, SignerAndSignature>().set(0n, sig);
+
+
+
+    // console.log(storeClaimData(claimData).toString());
+    
+    // await bridge.send(
+    //     provider.sender(),
+    //     {
+    //         value: toNano('0.3')
+    //     }, {
+    //     $$type: "ClaimNFT721",
+    //     data: claimData,
+    //     len: 1n,
+    //     signatures: dictA
+    // });
 
 
     // const Collection = provider.open(await NftCollection.fromInit(Address.parseFriendly("EQAV8tH2WDuWYU7zAmkJmIwP8Ph_uIC4zBqJNIfKgRUUQewh").address, newContent, {
@@ -51,28 +99,26 @@ export async function run(provider: NetworkProvider) {
 
     // // ===== Parameters =====
     // // Replace owner with your address
-    await bridge.send(
-        provider.sender(),
-        {
-            value: toNano('0.3'),
-        },
-        {
-            $$type: 'Gando', content: newContent, royalty: {
-                $$type: "RoyaltyParams",
-                denominator: 1000n,
-                destination: bridge.address,
-                numerator: 350n
-            }
-        }
-    );
+    // await bridge.send(
+    //     provider.sender(),
+    //     {
+    //         value: toNano('0.3'),
+    //     },
+    //     {
+    //         $$type: 'Gando', content: newContent, royalty: {
+    //             $$type: "RoyaltyParams",
+    //             denominator: 1000n,
+    //             destination: bridge.address,
+    //             numerator: 350n
+    //         }
+    //     }
+    // );
 
-    await provider.waitForDeploy(bridge.address);
+    // await provider.waitForDeploy(bridge.address);
 
 
-    // console.log(await bridge.getCollectionDeployer());
+    console.log(await bridge.getCollectionDeployer());
 
-    // console.log(await bridge.getStorageDeployer());
-
-    // console.log(await bridge.getCollections(1n));
+    console.log(await bridge.getStorageDeployer());
 
 }
--- ton/wrappers/Bridge.compile.ts
+++ ton/wrappers/Bridge.compile.ts
@@ -2,5 +2,5 @@ import { CompilerConfig } from '@ton-community/blueprint';
 
 export const compile: CompilerConfig = {
     lang: 'tact',
-    target: 'contracts/gando.tact',
+    target: 'contracts/bridge.tact',
 };
